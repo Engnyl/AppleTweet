2d80565122724e79d9d43fa298d80d22c59955f3
Engin Yıldız <engnyl@Engins-MacBook-Pro.local>
2017-10-23 19:36:17 +0300

    Initial Commit

---
+++ Tweets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweets
+//
+//  Created by Engin Yıldız on 23/10/2017.
+//  Copyright © 2017 Engin Yıldız. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
